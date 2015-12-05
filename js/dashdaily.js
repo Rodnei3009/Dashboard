427074ffa801090c801a52f600e6ead6a77456d9
Rodnei3009 <rbrassoroto@yahoo.com.br>
2015-12-05 20:19:35 -0200

    555
    555

--- js/dashdaily.js
+++ js/dashdaily.js
@@ -4,6 +4,14 @@ myApp.controller('dashitems', function($scope, $interval, dateFilter) {
     
     var valueToPush = new Array();
     
+    var registro = function(sistema, volume) {
+        this.sistema = sistema;
+        this.volume = volume
+    };
+    
+    var dados = [];
+    
+    
     $scope.dataSourceOri = [];
     $scope.dataSourceTOP5 = [];
     
@@ -42,15 +50,16 @@ myApp.controller('dashitems', function($scope, $interval, dateFilter) {
             $scope.dataSourceTOP5 = [];
             
             snapshot.forEach(function(childSnapshot) { //para cada chamado
-                    //alert(childSnapshot.child('abertura').val().toString().substring(0,8));
+
                     if (childSnapshot.child('abertura').val().toString().substring(0,8) === $scope.ano_mes_dia) {
+                        
                         $scope.vol_acum = $scope.vol_acum + 1;
                         no_prazo = no_prazo + childSnapshot.child('no_prazo').val();
                         reabertos = reabertos + childSnapshot.child('reaberto').val();
                         if  (childSnapshot.child('vcto').val().toString().substring(0,8) === hoje) {    
                             vcto_no_dia = vcto_no_dia + 1;
                         }                        
-                        
+                                                
                         $scope.dataSourceOri.push({sistema: childSnapshot.child('sistema').val().toString().substring(0,8), vol: 1});    
                         
                         sla =  no_prazo / $scope.vol_acum * 100;
@@ -61,11 +70,17 @@ myApp.controller('dashitems', function($scope, $interval, dateFilter) {
                     //$scope.produtos.push({'ID': childSnapshot.key(), 'descricao': childSnapshot.child('descricao').val(), 'categoria': childSnapshot.child('categoria').val(), 'status': childSnapshot.child('status').val()});
             });            
                         
-            $scope.dataSourceTOP5 = $scope.dataSourceOri;
+            var ordenado = groupBySistema($scope.dataSourceOri);            
+            
+            var dataNovo = new DevExpress.data.DataSource(ordenado);
+            
+            dataNovo.load().done(function(result) {
 
+            });
+            
             
             var dChart = $("#bar-5").dxChart("instance");
-            dChart.option({ dataSource: $scope.dataSourceTOP5 });
+            dChart.option({ dataSource: ordenado });
             dChart._render();
 
             $scope.$apply();
@@ -84,12 +99,55 @@ myApp.controller('dashitems', function($scope, $interval, dateFilter) {
         $scope.ano_mes_dia = moment($scope.ano_mes_dia, "YYYYMMDD").add(1, 'day').format('YYYYMMDD');
         $scope.carregar($scope.ano_mes_dia);
     };
+
+    function groupBySistema(data) {
+        
+        var result = [];
+        var cont = 0;
+        var encontrou = 0;
+        
+        for	(i = 0; i < data.length; i++) {
+            if(cont === 0){
+                result.push({sistema: data[i].sistema, vol: data[i].vol});
+                cont = 1;
+            } else {
+                //$scope.currentTime = data[i].sistema;
+                encontrou = 0;
+                for	(j = 0; j < result.length; j++) {
+                    if(data[i].sistema === result[j].sistema) {
+                        result[j].vol = result[j].vol + data[i].vol;
+                        //$scope.currentTime = data[i].sistema + ' - ' + result[j].sistema;
+                        //return result;
+                        encontrou = 1;
+                    }
+                }
+                if(encontrou === 0){
+                    result.push({sistema: data[i].sistema, vol: data[i].vol});
+                }
+            }            
+        }     
+        result.sort(sortArray);
+        return result;
+    };
+    
+    function sortArray(a, b) {
+        if(a.length === 0){
+            return 0;
+        }
+        
+        if (a.vol === b.vol) {
+            return 0;
+        }
+        else {
+            return (a.vol < b.vol) ? -1 : 1;
+        }
+    };
     
     //TOP 5 Sistemas
     //=============================================================================================================    
     $("#bar-5").dxChart({
         rotated: true,
-        dataSource: $scope.dataSourceOri,
+        dataSource: $scope.dataSourceOri.sort(0),
         commonSeriesSettings: {
             argumentField: "sistema",
             type: "stackedbar",
@@ -111,11 +169,16 @@ myApp.controller('dashitems', function($scope, $interval, dateFilter) {
         },
         pointClick: function(point) {
             point.isSelected() ? point.clearSelection() : point.select();
+        },
+        dataPrepareSettings: {
+           sortingMethod: true
         }
     });
     //TOP 5 Sistemas
     //=============================================================================================================
 
 });
+
+
 
 
