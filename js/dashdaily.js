e49e2a3ac50449f7941c9004d1829a4941635857
Rodnei3009 <rbrassoroto@yahoo.com.br>
2015-12-05 02:07:30 -0200

    05/12
    05/12

--- js/dashdaily.js
+++ js/dashdaily.js
@@ -1,7 +1,12 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('dashitems', function($scope, $interval, dateFilter) {
-                    
+    
+    var valueToPush = new Array();
+    
+    $scope.dataSourceOri = [];
+    $scope.dataSourceTOP5 = [];
+    
     $scope.ano_mes_dia = dateFilter(new Date(), 'yyyyMMdd');
     $scope.data_exibir = moment($scope.ano_mes_dia, "YYYYMMDD").format('DD MMMM YYYY');
     
@@ -32,8 +37,10 @@ myApp.controller('dashitems', function($scope, $interval, dateFilter) {
         
         myData = new Firebase("https://itdashboard.firebaseio.com/ambev/volpi/" + dat_carregar.substring(0,6));
         myData.on('value', function(snapshot){
-            //alert(snapshot.numChildren());
-            //$scope.vol_acum = snapshot.numChildren(); //total de chamados        
+            
+            $scope.dataSourceOri = [];
+            $scope.dataSourceTOP5 = [];
+            
             snapshot.forEach(function(childSnapshot) { //para cada chamado
                     //alert(childSnapshot.child('abertura').val().toString().substring(0,8));
                     if (childSnapshot.child('abertura').val().toString().substring(0,8) === $scope.ano_mes_dia) {
@@ -42,14 +49,24 @@ myApp.controller('dashitems', function($scope, $interval, dateFilter) {
                         reabertos = reabertos + childSnapshot.child('reaberto').val();
                         if  (childSnapshot.child('vcto').val().toString().substring(0,8) === hoje) {    
                             vcto_no_dia = vcto_no_dia + 1;
-                        }
+                        }                        
+                        
+                        $scope.dataSourceOri.push({sistema: childSnapshot.child('sistema').val().toString().substring(0,8), vol: 1});    
+                        
                         sla =  no_prazo / $scope.vol_acum * 100;
                         $scope.sla = sla.toFixed(1);
                         $scope.reabertos = reabertos;
                         $scope.vcto_no_dia = vcto_no_dia;
                     }
                     //$scope.produtos.push({'ID': childSnapshot.key(), 'descricao': childSnapshot.child('descricao').val(), 'categoria': childSnapshot.child('categoria').val(), 'status': childSnapshot.child('status').val()});
-            });
+            });            
+                        
+            $scope.dataSourceTOP5 = $scope.dataSourceOri;
+
+            var dChart = $("#bar-5").dxChart("instance");
+            dChart.option({ dataSource: $scope.dataSourceTOP5 });
+            dChart._render();
+
             $scope.$apply();
             $scope.carregou = false;
 
@@ -67,5 +84,37 @@ myApp.controller('dashitems', function($scope, $interval, dateFilter) {
         $scope.carregar($scope.ano_mes_dia);
     };
     
-    
+    //TOP 5 Sistemas
+    //=============================================================================================================    
+    $("#bar-5").dxChart({
+        rotated: true,
+        dataSource: $scope.dataSourceOri,
+        commonSeriesSettings: {
+            argumentField: "sistema",
+            type: "stackedbar",
+            selectionStyle: {
+                hatching: {
+                    direction: "left"
+                }
+            }
+        },
+        series: [
+            { valueField: "vol", name: "Volume", color: "#ffd700" }
+        ],
+        title: {
+            text: "Top 5 sistemas"
+        },
+        legend: {
+            verticalAlignment: "bottom",
+            horizontalAlignment: "center"
+        },
+        pointClick: function(point) {
+            point.isSelected() ? point.clearSelection() : point.select();
+        }
+    });
+    //TOP 5 Sistemas
+    //=============================================================================================================
+
 });
+
+
